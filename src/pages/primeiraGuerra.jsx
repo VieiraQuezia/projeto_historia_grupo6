06dafe9df3cadfea07f7b89d31869ff7b8049b56
Leonardo <leomori3107@gmail.com>
2025-05-15 11:50:15 -0300

    pags add guerraContest, primeiraGuerra e rev30

---
+++ src/pages/primeiraGuerra.jsx
@@ -0,0 +1,99 @@
+import "../pages/zpages.css";
+
+// import das imagens
+import Img1 from "../assets/conflitosSecXX/primeiraGuerra/img2.webp";
+import Img2 from "../assets/conflitosSecXX/primeiraGuerra/img4.webp";
+import Img3 from "../assets/conflitosSecXX/primeiraGuerra/img5.jpg";
+
+// componentes
+import NavAnuncio from "../components/cardAnuncio";
+import Header from "../components/header";
+import Footer from "../components/footer";
+const CardComNav = () => {
+  return (
+    <>
+      <Header titulo="Primeira Guerra Mundial" />
+      <div className="container">
+        <NavAnuncio />
+
+        {/* Card principal */}
+        <nav className="main-nav">
+          <div className="card">
+            <img src={Img1} alt="Imagem fixa" className="static-img" />
+
+            <div className="card-content">
+              <p className="card-text">
+                A Primeira Guerra Mundial, que ocorreu entre 1914 e 1918, foi um
+                conflito de escala mundial, envolvendo as grandes potências
+                econômicas e militares da época. Os antecedentes remontam até o
+                século XIX, quando as disputas imperialistas por colônias na
+                África e na Ásia se intensificaram. Além disso, havia uma
+                rivalidade crescente entre as nações europeias, como a
+                rivalidade anglo-alemã e a franco-alemã, alimentada pelo
+                nacionalismo. As alianças militares complexas também
+                desempenharam um papel crucial, com a formação da Tríplice
+                Aliança (Alemanha, Áustria-Hungria e Itália) e da Tríplice
+                Entente (França, Inglaterra e Rússia). Esse período anterior à
+                guerra é conhecido como Paz Armada, caracterizado por altos
+                investimentos em armamentos e pela tensão crescente. O estopim
+                foi o assassinato do arquiduque Francisco Ferdinando, herdeiro
+                do trono austro-húngaro, em Sarajevo, no dia 28 de junho de
+                1914, feito por um militante sérvio. Esse ato desencadeou uma
+                onda de declarações de guerra, impulsionadas pelas alianças
+                estabelecidas entre os países.
+              </p>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src={Img2} alt="Imagem fixa" className="imgespecifico" />
+
+            <div className="card-content">
+              <p className="card-text">
+                A Primeira Guerra Mundial se desenrolou em duas fases
+                principais. A primeira, a guerra de movimento, foi marcada por
+                rápidos avanços e tentativas de ocupação territorial. Já a
+                segunda, a guerra de trincheiras, dominou grande parte do
+                conflito na Frente Ocidental. As trincheiras, extensas redes de
+                buracos, tornaram-se um símbolo da brutalidade e do impasse da
+                guerra, com milhões de soldados vivendo em condições insalubres
+                e enfrentando ataques constantes. Neste contexto, novas
+                tecnologias de guerra, como metralhadoras, gases tóxicos,
+                tanques e aviões, foram utilizadas em larga escala, resultando
+                em diversas mortes e feridos. Em 1917, dois eventos
+                significativos mudaram o rumo da guerra: a saída da Rússia, em
+                função da Revolução Russa, e a entrada dos Estados Unidos ao
+                lado da Entente. A participação dos EUA, com seu potencial
+                humano e industrial, foi fundamental para a vitória dos Aliados.
+              </p>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src={Img3} alt="Imagem fixa" className="static-img" />
+
+            <div className="card-content">
+              <p className="card-text">
+                A guerra chegou ao fim em 11 de novembro de 1918, com a derrota
+                das Potências Centrais. Em 1919, foi assinado o Tratado de
+                Versalhes, que impôs duras condições à Alemanha, considerada a
+                principal responsável pelo conflito. Esse tratado gerou um clima
+                de instabilidade e ressentimento que eventualmente contribuiria
+                para o surgimento da Segunda Guerra Mundial. A Primeira Guerra
+                Mundial deixou um rastro devastador, com milhões de mortos e
+                feridos. Ela também redesenhou o mapa da Europa, resultando no
+                fim de impérios como o Austro-Húngaro, o Otomano e o Russo,
+                marcando o declínio da Europa e a ascensão dos Estados Unidos
+                como uma potência global.
+              </p>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default CardComNav;
---
+++ src/pages/revolucao30.jsx
@@ -0,0 +1,72 @@
+import "../pages/zpages.css";
+
+// import das imagens
+import Img1 from "../assets/conflitosSecXX/revolucao1930/img1.jpg";
+import Img2 from "../assets/conflitosSecXX/revolucao1930/img2.jpg";
+
+// componentes
+import NavAnuncio from "../components/cardAnuncio";
+import Header from "../components/header";
+import Footer from "../components/footer";
+const CardComNav = () => {
+  return (
+    <>
+      <Header titulo="Revolução de 1930" />
+      <div className="container">
+        <NavAnuncio />
+
+        {/* Card principal */}
+        <nav className="main-nav">
+          <div className="card">
+            <img src={Img1} alt="Imagem fixa" className="static-img" />
+
+            <div className="card-content">
+              <p className="card-text">
+                A Revolução de 1930 foi um movimento armado de alcance nacional,
+                liderado por Getúlio Vargas, então governador do Rio Grande do
+                Sul. Esse levante resultou na deposição do presidente Washington
+                Luís e impediu a posse do presidente eleito Júlio Prestes,
+                encerrando a chamada República Oligárquica e iniciando o Governo
+                Provisório sob o comando de Vargas. As principais causas incluem
+                a Política do Café com Leite, acordo informal de alternância no
+                poder entre as oligarquias de São Paulo (produtores de café) e
+                Minas Gerais (produtores de leite), dominando a presidência do
+                país. Em 1930, o presidente Washington Luís, paulista, rompeu o
+                pacto ao apoiar outro paulista, Júlio Prestes, como sucessor,
+                contrariando as expectativas de Minas Gerais. Em resposta, Minas
+                Gerais, Rio Grande do Sul e Paraíba formaram a Aliança Liberal,
+                lançando Getúlio Vargas à presidência e João Pessoa como vice.
+                Além disso, os demais estados brasileiros estavam crescendo em
+                importância e reivindicando mais espaço no cenário político
+                brasileiro. O estopim da revolução foi o assassinato do
+                candidato a vice-presidente João Pessoa, em julho de 1930.
+              </p>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src={Img2} alt="Imagem fixa" className="static-img" />
+
+            <div className="card-content">
+              <p className="card-text">
+                A revolução teve início em 3 de outubro de 1930, com confrontos
+                entre forças revolucionárias e tropas federais. Em menos de um
+                mês, os revolucionários conseguiram depor Washington Luís. Em 3
+                de novembro, Getúlio Vargas assumiu a liderança do Governo
+                Provisório, que duraria até 1934. A revolução encerrou o domínio
+                das oligarquias estaduais sobre a política nacional; Vargas
+                iniciou um processo de centralização administrativa, reduzindo a
+                autonomia dos estados, além do incentivo à industrialização e à
+                modernização do país.
+              </p>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default CardComNav;
