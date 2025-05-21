cf99159a7e411fafdc618775de96e895d1d42283
Leonardo <leomori3107@gmail.com>
2025-05-21 13:16:53 -0300

    producaoAutoral add

--- src/pages/guerraContestado.jsx
+++ src/pages/guerraContestado.jsx
@@ -1,7 +1,3 @@
-
-
-
-
 import "./zpages.css"
  // Import das imagens
  import Img1 from "../assets/conflitosSecXX/guerraContestado/img1.jpg";
@@ -27,40 +23,10 @@ function Home(){
             <Header titulo='Guerra do Contestado' />
           
 
-<CardText 
- texto='      A Guerra do Contestado foi um conflito ocorrido de 1912 a 1916
-                 na divisa entre Santa Catarina e Paraná por disputas de terra.
-                 Recebeu esse nome pois era uma região contestada, ou seja,
-                 disputada, devido à abundância de erva-mate. Era projetada a
-                 construção de uma estrada de ferro nela, motivo pelo qual
-                 milhares de pessoas se deslocaram para a área, na expectativa de
-                 trabalhar na obra. Após a inauguração, houve uma crise social
-                 causada pelo desemprego dos habitantes recém chegados, que se
-                 juntaram em comunidades formadas por monges messiânicos. O
-                 principal deles era José Maria do Santo Agostinho, um
-                 antirrepublicano que pregava o fim do mundo nos anos 2000,
-                 gerando medo e obediência dos seguidores. Seu fortalecimento
-                 como líder causou incômodo na Igreja Católica e coronéis. O
-                 governo federal, preocupado com a formação das comunidades,
-                 enviou tropas para a região, as quais não obtiveram vitória
-                 contra os sertanejos.' 
- img={Img1}  />
-<CardText 
- texto='      Com a perda das terras após a inauguração da estrada, houve uma
-                 revolta por parte dos trabalhadores, iniciando um levante armado
-                 liderado por José Maria em busca de um governo independente. Em
-                 reação, o então presidente da república Hermes da Fonseca enviou
-                 tropas, em uma tentativa frustrada de derrotar a comunidade do
-                 Quadrado Santo, onde José Maria vivia com seus seguidores. Logo
-                 nos primeiros confrontos, o monge foi assassinado, porém a
-                 guerra continuou com o eventual sucesso das tropas do governo em
-                 dizimar os seguidores, sendo possível após inúmeros confrontos
-                 que envolveram armamentos pesados e aviões de artilharia. As
-                 questões envolvendo a área da divisa foram resolvidas por meio
-                 de um acordo assinado entre paranaenses e catarinenses,
-                 principal consequência da guerra.' 
- img={Img2}  />
-
+            <div className="card-content">
+              <video src=""></video>
+            </div>
+                      <BtnCurtida id="Fascismo-Italiano" />
 
 
 <BtnCurtida/>
