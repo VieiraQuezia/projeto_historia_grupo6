31c6bc62ce174e19f2d5d5ada73ccfa3bb0b3958
Guih <ggsantana18@gmail.com>
2025-05-22 08:56:10 -0300

    att header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,22 @@
-
 import { Link } from "react-router-dom";
-
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./header.css";
-
 import BtnEscuro from "./botaoEscuro";
-
-import logo from "../assets/logoSeculoChamas.png"; // Importe a imagem do logo
+import logo from "../assets/logoSeculoChamas.png";
 
 function Header(props) {
-   
-
     const [menuAberto, setMenuAberto] = useState(false);
-    const menuRef = useRef(null);
-    const iconRef = useRef(null);
 
-  
+    const toggleMenu = () => {
+        setMenuAberto((prev) => !prev);
+    };
 
-    // Fecha o menu se clicar fora
     useEffect(() => {
         const handleClickFora = (event) => {
-            if (
-                menuRef.current &&
-                !menuRef.current.contains(event.target) &&
-                iconRef.current &&
-                !iconRef.current.contains(event.target)
-            ) {
+            const isMenu = event.target.closest(".menu");
+            const isIcon = event.target.closest(".menu-icon");
+
+            if (!isMenu && !isIcon) {
                 setMenuAberto(false);
             }
         };
@@ -36,40 +27,32 @@ function Header(props) {
         };
     }, []);
 
-    const toggleMenu = () => {
-        setMenuAberto((prev) => !prev);
-    };
-
     return (
         <>
             <header className="headerBox">
-                   <img className="Logo" src={logo} alt="Logo" /> 
-<h1><Link to='/'>{props.titulo}</Link></h1>              
-  <i
-                    ref={iconRef}
+                <img className="Logo" src={logo} alt="Logo" />
+                <h1><Link to="/">{props.titulo}</Link></h1>
+                <i
+                    className="bx bx-menu menu-icon"
                     onClick={toggleMenu}
-                    className="bx bx-menu"
                 ></i>
             </header>
 
             {menuAberto && (
-                <nav className="menu" ref={menuRef}>
+                <nav className="menu">
                     <ul>
-
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/canudos">Guerra de Canudos</Link></li>
-             <li><Link to="/contestado">Guerra do Contestado</Link></li>
-             <li><Link to="/primeira-guerra">Primeira Guerra Mundial</Link></li>
-             <li><Link to="/revolucao-russa">Revolução Russa</Link></li>
-             <li><Link to="/fascismo">Fascismo Italiano</Link></li>
-             <li><Link to="/crise-29">Crise de 1929</Link></li>
-             <li><Link to="/revolucao-1930">Revolução de 1930</Link></li>
-             <li><Link to="/ranking">Ranking </Link></li>
-                                     <li><Link to='/producaoautoral'>Produção Autoral - Crise de 1929</Link></li>
-                                            <BtnEscuro />
-
+                        <li><Link to="/contestado">Guerra do Contestado</Link></li>
+                        <li><Link to="/primeira-guerra">Primeira Guerra Mundial</Link></li>
+                        <li><Link to="/revolucao-russa">Revolução Russa</Link></li>
+                        <li><Link to="/fascismo">Fascismo Italiano</Link></li>
+                        <li><Link to="/crise-29">Crise de 1929</Link></li>
+                        <li><Link to="/revolucao-1930">Revolução de 1930</Link></li>
+                        <li><Link to="/ranking">Ranking</Link></li>
+                        <li><Link to="/producaoautoral">Produção Autoral - Crise de 1929</Link></li>
+                        <BtnEscuro />
                     </ul>
-
                 </nav>
             )}
         </>
